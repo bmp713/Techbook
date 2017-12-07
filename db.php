c2da2feec4c537613cfd04344a72c7cfcd7ebb8a
Brandon Michael Piper <bmp713@yahoo.com>
2017-12-07 02:18:18 -0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,13 @@
+
+<?php
+/* Login script for MySQL database */
+$host = "localhost";
+$username = "brandon";
+$password = "Password123";
+$database = "db";
+$table = "users";
+
+/* Connect to mySQL */
+$connect = mysqli_connect( $host, $username, $password, $database )
+		or die("db.php: Unable to connect to MySQL");
+?>
